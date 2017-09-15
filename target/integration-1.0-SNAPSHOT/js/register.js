c1ca3d15db589d3bcd243431561d76fe4ffd525e
piekatoski <l.piekatoski@gmail.com>
2017-09-15 19:16:44 -0300

    POM

---
+++ target/integration-1.0-SNAPSHOT/js/register.js
@@ -0,0 +1,18 @@
+/*function Register(){
+   location.href="register.html";
+}*/
+
+function ConfirmRegister(){
+    alert("Cadastro realizado com sucesso!");
+    return true;
+}
+
+function Logon(){
+    if (document.getElementById("username").value == "leandro" ){
+        return true;
+    }else{
+        alert("Usuario ou senha invalidos!");
+        return false;
+    }
+
+}
